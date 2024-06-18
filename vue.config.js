d20b06d10659fd0cdeb0b9bb939a6730f044eb42
Java_Rookie <9241851+java_rookie49@user.noreply.gitee.com>
2024-06-18 09:46:24 +0800

    publicPath: / -> ./

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/vuejs-essential/dist/'
-    : '/'
+    : './'
 }
 
 module.exports = {
