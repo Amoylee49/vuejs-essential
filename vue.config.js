a4a7866353983466613331c02dd06a1da22e5166
Java_Rookie <9241851+java_rookie49@user.noreply.gitee.com>
2024-06-18 11:24:28 +0800

    config

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,12 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/vuejs-essential/dist/'
-    : '/vuejs-essential/dist/'
+    ? '././'
+    : '/',
+    outputDir: 'dist',
  /**
    * publicPath 默认是 / 是根路径，这个是指服务的根路径：https://xxx.github.io/，发布后会从这个路径下找 js.css 等资源，而生成的网站路径是这个 https://xxx.github.io/Vue-Element/，显然是找不到的
    * 我们需要修改为 相对路径'./' 或是‘.’ 或是 直接设置的项目子路径 :/项目名称/ 就可找到资源了
@@ -17,6 +19,7 @@ module.exports = {
   // productionSourceMap: false,
 
 // 链接：https://juejin.cn/post/7189860636661121084
+// https://jgckm.github.io/posts/23840d68.html#%E2%9A%99%E8%A7%A3%E5%86%B3%E9%97%AE%E9%A2%98
 
 }
 
